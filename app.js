a2345180a13bf3949869c21479009a178f82e85b
Mohammad Mansour <mohammed.i02@hotmail.com>
2023-09-29 03:11:10 +0300

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/note-keeping-app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
