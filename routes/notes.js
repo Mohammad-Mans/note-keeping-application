62ca04a0aa608179eb33911bc773fd30acc716f5
Mohammad Mansour <mohammed.i02@hotmail.com>
2023-09-29 19:57:53 +0300

    create GET endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -2,4 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Note = require("../models/Note");
 
+router.get("/", async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
